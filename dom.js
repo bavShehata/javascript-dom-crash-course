21d170b22677ae0f956902ef829e0c0ab180165b
saddamarbaa <saddamarbaas@gmail.com>
2020-11-29 06:15:16 +0700

    getElementById selector

--- dom.js
+++ dom.js
@@ -42,6 +42,32 @@ console.log(document.links);
 // thsi will give us the first form) 
 console.log(document.forms[0]);
 
-
 // thsi will give us images) 
 console.log(document.images);
+
+
+// SELECTORS//
+
+// GETELEMENTBYID
+
+let headerTitle = document.getElementById('header-title');
+
+let header = document.getElementById('main-header')
+
+console.log(headerTitle);
+console.log(header);
+
+// textContent
+
+// headerTitle.textContent = "hello";
+
+// innerText
+
+// headerTitle.innerText = "goodby";
+
+// innerHTML
+
+// headerTitle.innerHTML = "goodby";
+
+// change the style 
+header.style.borderBottom = 'solid 1px red';
