2499f96914eb1e40feae6fb4b5ee267d65067658
saddamarbaa <saddamarbaas@gmail.com>
2020-12-10 02:14:22 +0700

    Learn JS HTML Dom In Arabic #02 - Find Elements By [ ID, Class, Tag ]

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert("ok")
+// [1] - document.getElementById ()
+
+let myDiv = document.getElementById('test')
+myDiv.innerHTML = 'Changed by javascript'
+
+//  [2] - document.getElementsByTagName()
+
+myDiv = document.getElementsByTagName('p')
+myDiv[0].innerHTML = 12
+myDiv[1].innerHTML = 'Changed by javascript'
+
+// [3] -  document.getElementsByClassName()
+
+myDiv = document.getElementsByClassName('testDiv')
+myDiv[0].innerHTML = 'test with Javscript'
+myDiv[1].innerHTML = 'test with Javscript'
+
+
+// [4] -  document.querySelectorAll
+
+let myElement = document.querySelectorAll('div')
+console.log(myElement)
+myElement[1].innerHTML = 123445;
+myElement[0].innerHTML = 'Changed by javascript second test'
