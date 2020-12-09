7eea8ff63cbf2faee4ed41151bb2c24c6b32b0b0
saddamarbaa <saddamarbaas@gmail.com>
2020-12-10 02:41:42 +0700

    Find Elements By Objects

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 // [1] - document.getElementById ()
 
-let myDiv = document.getElementById('test')
-myDiv.innerHTML = 'Changed by javascript'
+// let myDiv = document.getElementById('test')
+// myDiv.innerHTML = 'Changed by javascript'
 
 //  [2] - document.getElementsByTagName()
 
-myDiv = document.getElementsByTagName('p')
-myDiv[0].innerHTML = 12
-myDiv[1].innerHTML = 'Changed by javascript'
+// myDiv = document.getElementsByTagName('p')
+// myDiv[0].innerHTML = 12
+// myDiv[1].innerHTML = 'Changed by javascript'
 
 // [3] -  document.getElementsByClassName()
 // Not Compatible With IE 8
 
-myDiv = document.getElementsByClassName('testDiv')
-myDiv[0].innerHTML = 'test with Javscript'
-myDiv[1].innerHTML = 'test with Javscript'
+// myDiv = document.getElementsByClassName('testDiv')
+// myDiv[0].innerHTML = 'test with Javscript'
+// myDiv[1].innerHTML = 'test with Javscript'
 
 
 // [4] -  document.querySelectorAll
 // Not Compatible With IE 8
 
-let myElement = document.querySelectorAll('div')
-console.log(myElement)
-myElement[1].innerHTML = 123445;
-myElement[0].innerHTML = 'Changed by javascript second test'
+// let myElement = document.querySelectorAll('div')
+// console.log(myElement)
+// myElement[1].innerHTML = 123445;
+// myElement[0].innerHTML = 'Changed by javascript second test'
+
+
+// Object Collection 
+// [5] -  document.title
+// [6] - document.images
+// [7] document.forms
+// [9] document.body
+// [10] document.links
+
+let myDiv = document.getElementById('test')
+myDiv.innerHTML = document.title
+console.log(document.title)
